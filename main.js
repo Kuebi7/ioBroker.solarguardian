9bbd03ef3d00e6bdd1affe8fc0678f159bfe91b1
Kuebi7 <andreas.kueberl@outlook.com>
2025-03-25 15:35:11 +0100

    Update main.js
    Grok

--- main.js
+++ main.js
@@ -1,168 +1,116 @@
 "use strict";
 
-/*
- * Created with @iobroker/create-adapter v2.6.5
- */
-
-// The adapter-core module gives you access to the core ioBroker functions
-// you need to create an adapter
 const utils = require("@iobroker/adapter-core");
-
-// Load your modules here, e.g.:
-// const fs = require("fs");
+const axios = require("axios");
 
 class Solarguardian extends utils.Adapter {
+  constructor(options) {
+    super({
+      ...options,
+      name: "solarguardian",
+    });
+    this.on("ready", this.onReady.bind(this));
+    this.on("unload", this.onUnload.bind(this));
+  }
 
-	/**
-	 * @param {Partial<utils.AdapterOptions>} [options={}]
-	 */
-	constructor(options) {
-		super({
-			...options,
-			name: "solarguardian",
-		});
-		this.on("ready", this.onReady.bind(this));
-		this.on("stateChange", this.onStateChange.bind(this));
-		// this.on("objectChange", this.onObjectChange.bind(this));
-		// this.on("message", this.onMessage.bind(this));
-		this.on("unload", this.onUnload.bind(this));
-	}
+  async onReady() {
+    this.setState("info.connection", false, true);
 
-	/**
-	 * Is called when databases are connected and adapter received configuration.
-	 */
-	async onReady() {
-		// Initialize your adapter here
+    // Konfigurationswerte auslesen
+    const { appKey, appSecret, pollInterval } = this.config;
 
-		// The adapters config (in the instance object everything under the attribute "native") is accessible via
-		// this.config:
-		this.log.info("config appKey: " + this.config.appKey);
-		this.log.info("config appSecret: " + this.config.appSecret);
-		this.log.info("config pollInterval: " + this.config.pollInterval);
+    if (!appKey || !appSecret) {
+      this.log.error("appKey und appSecret müssen in der Konfiguration angegeben werden!");
+      return;
+    }
 
-		/*
-		For every state in the system there has to be also an object of type state
-		Here a simple template for a boolean variable named "testVariable"
-		Because every adapter instance uses its own unique namespace variable names can't collide with other adapters variables
-		*/
-		await this.setObjectNotExistsAsync("testVariable", {
-			type: "state",
-			common: {
-				name: "testVariable",
-				type: "boolean",
-				role: "indicator",
-				read: true,
-				write: true,
-			},
-			native: {},
-		});
+    try {
+      // Token abrufen
+      const token = await this.getAuthToken(appKey, appSecret);
+      this.setState("info.connection", true, true);
+      this.log.info("Erfolgreich mit Solarguardian API verbunden");
 
-		// In order to get state updates, you need to subscribe to them. The following line adds a subscription for our variable we have created above.
-		this.subscribeStates("testVariable");
-		// You can also add a subscription for multiple states. The following line watches all states starting with "lights."
-		// this.subscribeStates("lights.*");
-		// Or, if you really must, you can also watch all states. Don't do this if you don't need to. Otherwise this will cause a lot of unnecessary load on the system:
-		// this.subscribeStates("*");
+      // Daten abrufen und speichern
+      await this.fetchData(token);
 
-		/*
-			setState examples
-			you will notice that each setState will cause the stateChange event to fire (because of above subscribeStates cmd)
-		*/
-		// the variable testVariable is set to true as command (ack=false)
-		await this.setStateAsync("testVariable", true);
+      // Regelmäßiges Polling einrichten
+      this.pollingInterval = setInterval(() => this.fetchData(token), pollInterval);
+    } catch (error) {
+      this.log.error(`Fehler bei der Initialisierung: ${error.message}`);
+      this.setState("info.connection", false, true);
+    }
+  }
 
-		// same thing, but the value is flagged "ack"
-		// ack should be always set to true if the value is received from or acknowledged from the target system
-		await this.setStateAsync("testVariable", { val: true, ack: true });
+  async getAuthToken(appKey, appSecret) {
+    const url = "https://openapi.epsolarpv.com/epCloud/user/getAuthToken";
+    const response = await axios.post(
+      url,
+      { appKey, appSecret },
+      { headers: { "Content-Type": "application/json" } }
+    );
 
-		// same thing, but the state is deleted after 30s (getState will return null afterwards)
-		await this.setStateAsync("testVariable", { val: true, ack: true, expire: 30 });
+    if (response.data.status !== 0) {
+      throw new Error(`Token-Anfrage fehlgeschlagen: ${response.data.info}`);
+    }
 
-		// examples for the checkPassword/checkGroup functions
-		let result = await this.checkPasswordAsync("admin", "iobroker");
-		this.log.info("check user admin pw iobroker: " + result);
+    return response.data.data["X-Access-Token"];
+  }
 
-		result = await this.checkGroupAsync("admin", "admin");
-		this.log.info("check group user admin group admin: " + result);
-	}
+  async fetchData(token) {
+    try {
+      // Beispiel: Kraftwerksliste abrufen
+      const url = "https://openapi.epsolarpv.com/epCloud/vn/openApi/getPowerStationListPage";
+      const response = await axios.post(
+        url,
+        { powerStationName: "", pageNo: 1, pageSize: 10 },
+        {
+          headers: {
+            "Content-Type": "application/json",
+            "X-Access-Token": token,
+          },
+        }
+      );
 
-	/**
-	 * Is called when adapter shuts down - callback has to be called under any circumstances!
-	 * @param {() => void} callback
-	 */
-	onUnload(callback) {
-		try {
-			// Here you must clear all timeouts or intervals that may still be active
-			// clearTimeout(timeout1);
-			// clearTimeout(timeout2);
-			// ...
-			// clearInterval(interval1);
+      if (response.data.status === 0) {
+        const powerStations = response.data.data.list;
+        this.log.info(`Erfolgreich ${powerStations.length} Kraftwerke abgerufen`);
 
-			callback();
-		} catch (e) {
-			callback();
-		}
-	}
+        // Daten in ioBroker speichern
+        for (const station of powerStations) {
+          const basePath = `powerStations.${station.id}`;
+          await this.setObjectNotExistsAsync(`${basePath}.name`, {
+            type: "state",
+            common: { name: "Name", type: "string", role: "value", read: true, write: false },
+            native: {},
+          });
+          await this.setObjectNotExistsAsync(`${basePath}.alarmStatus`, {
+            type: "state",
+            common: { name: "Alarm Status", type: "number", role: "value", read: true, write: false },
+            native: {},
+          });
+          await this.setStateAsync(`${basePath}.name`, station.powerStationName, true);
+          await this.setStateAsync(`${basePath}.alarmStatus`, station.alarmStatus, true);
+        }
+      } else {
+        this.log.warn(`Datenabfrage fehlgeschlagen: ${response.data.info}`);
+      }
+    } catch (error) {
+      this.log.error(`Fehler beim Abrufen der Daten: ${error.message}`);
+    }
+  }
 
-	// If you need to react to object changes, uncomment the following block and the corresponding line in the constructor.
-	// You also need to subscribe to the objects with `this.subscribeObjects`, similar to `this.subscribeStates`.
-	// /**
-	//  * Is called if a subscribed object changes
-	//  * @param {string} id
-	//  * @param {ioBroker.Object | null | undefined} obj
-	//  */
-	// onObjectChange(id, obj) {
-	// 	if (obj) {
-	// 		// The object was changed
-	// 		this.log.info(`object ${id} changed: ${JSON.stringify(obj)}`);
-	// 	} else {
-	// 		// The object was deleted
-	// 		this.log.info(`object ${id} deleted`);
-	// 	}
-	// }
-
-	/**
-	 * Is called if a subscribed state changes
-	 * @param {string} id
-	 * @param {ioBroker.State | null | undefined} state
-	 */
-	onStateChange(id, state) {
-		if (state) {
-			// The state was changed
-			this.log.info(`state ${id} changed: ${state.val} (ack = ${state.ack})`);
-		} else {
-			// The state was deleted
-			this.log.info(`state ${id} deleted`);
-		}
-	}
-
-	// If you need to accept messages in your adapter, uncomment the following block and the corresponding line in the constructor.
-	// /**
-	//  * Some message was sent to this instance over message box. Used by email, pushover, text2speech, ...
-	//  * Using this method requires "common.messagebox" property to be set to true in io-package.json
-	//  * @param {ioBroker.Message} obj
-	//  */
-	// onMessage(obj) {
-	// 	if (typeof obj === "object" && obj.message) {
-	// 		if (obj.command === "send") {
-	// 			// e.g. send email or pushover or whatever
-	// 			this.log.info("send command");
-
-	// 			// Send response in callback if required
-	// 			if (obj.callback) this.sendTo(obj.from, obj.command, "Message received", obj.callback);
-	// 		}
-	// 	}
-	// }
-
+  onUnload(callback) {
+    if (this.pollingInterval) {
+      clearInterval(this.pollingInterval);
+    }
+    this.setState("info.connection", false, true);
+    this.log.info("Adapter wird beendet");
+    callback();
+  }
 }
 
 if (require.main !== module) {
-	// Export the constructor in compact mode
-	/**
-	 * @param {Partial<utils.AdapterOptions>} [options={}]
-	 */
-	module.exports = (options) => new Solarguardian(options);
+  module.exports = (options) => new Solarguardian(options);
 } else {
-	// otherwise start the instance directly
-	new Solarguardian();
+  new Solarguardian();
 }
